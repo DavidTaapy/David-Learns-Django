dfc304789872f6a64febca35ff372dbaa3a45465
DavidTaapy <DavidTayAP@Hotmail.Com>
2021-08-04 21:20:54 +0800

    Adding Positions DataFrame

--- src/sales/views.py
+++ src/sales/views.py
@@ -10,6 +10,7 @@ import pandas as pd
 def home_view(request):
     # Initializing dataframe
     sales_df = None
+    positions_df = None
     # Both with and without inputs
     form = SalesSearchForm(request.POST or None)
 
@@ -18,18 +19,32 @@ def home_view(request):
         date_from = request.POST.get('date_from')
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
-        qs = Sale.objects.filter(created__date__lte=date_to,
-                                 created__date__gte=date_from)
-
-        if len(qs) > 0:
-            sales_df = pd.DataFrame(qs.values())
+        sales_qs = Sale.objects.filter(created__date__lte=date_to,
+                                       created__date__gte=date_from)
+        if len(sales_qs) > 0:
+            sales_df = pd.DataFrame(sales_qs.values())
+            # Getting Positions
+            positions_data = []
+            for sale in sales_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        'position_id': pos.id,
+                        'product': pos.product.name,
+                        'quantity': pos.quantity,
+                        'price': pos.price
+                    }
+                    positions_data.append(obj)
+            positions_df = pd.DataFrame(positions_data)
+            # Converting DataFrames To HTML
             sales_df = sales_df.to_html()
+            positions_df = positions_df.to_html()
         else:
             print('No data')
 
     context = {
         'form': form,
-        'sales_df': sales_df
+        'sales_df': sales_df,
+        'positions_df': positions_df
     }
     return render(request, 'sales/home.html', context)
 
