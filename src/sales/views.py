042743bdf1d54164121555ce61924745282fe39b
DavidTaapy <DavidTayAP@Hotmail.Com>
2021-08-05 11:49:34 +0800

    Setting up charts
    - Barplot using seaborn
    - Piechart and linechart using matplotlib

--- src/sales/views.py
+++ src/sales/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
 from.forms import SalesSearchForm
-from .utils import get_customer_from_id, get_salesman_from_id
+from .utils import get_customer_from_id, get_salesman_from_id, get_chart
 import pandas as pd
 
 
@@ -13,6 +13,8 @@ def home_view(request):
     positions_df = None
     merged_df = None
     df = None
+    # Initializing chart
+    chart = None
     # Both with and without inputs
     form = SalesSearchForm(request.POST or None)
     # When information is given
@@ -52,6 +54,8 @@ def home_view(request):
             # Transforming Merged DataFrame
             df = merged_df.groupby('transaction_id', as_index=False)[
                 'price'].agg('sum')
+            # Generating charts
+            chart = get_chart(chart_type, df, labels=df['transaction_id'])
             # Converting DataFrames To HTML
             sales_df = sales_df.to_html()
             positions_df = positions_df.to_html()
@@ -65,7 +69,8 @@ def home_view(request):
         'sales_df': sales_df,
         'positions_df': positions_df,
         'merged_df': merged_df,
-        'df': df
+        'df': df,
+        'chart': chart
     }
     return render(request, 'sales/home.html', context)
 
