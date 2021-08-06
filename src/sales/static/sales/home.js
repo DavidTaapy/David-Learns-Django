88533c113a8d6718f5bd86cbad7d7530bf49dc09
DavidTaapy <DavidTayAP@Hotmail.Com>
2021-08-06 22:46:23 +0800

    Adding chart to the modal

--- src/sales/static/sales/home.js
+++ src/sales/static/sales/home.js
@@ -1,7 +1,13 @@
 // Getting required elements
 const reportBtn = document.getElementById('report-btn');
 const img = document.getElementById('img');
+const modalBody = document.getElementById('modal-body');
 
 if (img) {
     reportBtn.classList.remove('not-visible');
 }
+
+reportBtn.addEventListener('click', () => {
+    img.setAttribute('class', 'w-100');
+    modalBody.prepend(img);
+})
