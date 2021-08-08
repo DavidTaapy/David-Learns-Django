72ea105842f7b2785390a5f122a17f3493678fdc
DavidTaapy <DavidTayAP@Hotmail.Com>
2021-08-08 15:25:43 +0800

    Finishing Dropzone

--- src/reports/static/reports/upload.js
+++ src/reports/static/reports/upload.js
@@ -1,12 +1,30 @@
 const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+const alertBox = document.getElementById('alert-box');
+
+// Handling Alerts
+const handleAlerts = (type, msg) => {
+    alertBox.innerHTML = `
+        <div class="alert alert-${type}" role="alert">
+        ${msg}
+        </div>
+    `
+}
 
 Dropzone.autoDiscover = false;
 const myDropzone = new Dropzone('#my-dropzone', {
-    url: '/upload/',
+    url: '/reports/upload/',
     init: function() {
         this.on('sending', function(file, xhr, formData){
             console.log('sending');
             formData.append('csrfmiddlewaretoken', csrf);
+        })
+        this.on('success', function(file, response) {
+            const ex = response.ex;
+            if (ex) {
+                handleAlerts('danger', 'File already exists!');
+            } else {
+                handleAlerts('success', 'File uploaded!');
+            }
         })
     },
     maxFiles: 3,
--- src/reports/views.py
+++ src/reports/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from profiles.models import Profile
+from products.models import Product
+from customers.models import Customer
 from django.http import JsonResponse, HttpResponse
 from .utils import get_report_image
 from .models import Report
@@ -7,6 +9,9 @@ from django.views.generic import ListView, DetailView, TemplateView
 from django.conf import settings
 from django.template.loader import get_template
 from xhtml2pdf import pisa
+from sales.models import Sale, Position, CSV
+import csv
+from django.utils.dateparse import parse_date
 
 
 # Class for views
@@ -67,4 +72,38 @@ def render_pdf_view(request, pk):
 
 
 def csv_upload_view(request):
+    if request.method == 'POST':
+        csv_file_name = request.FILES.get('file').name
+        csv_file = request.FILES.get('file')
+        obj, created = CSV.objects.get_or_create(file_name=csv_file_name)
+        if created:
+            obj.csv_file = csv_file
+            obj.save()
+            with open(obj.csv_file.path, 'r') as f:
+                reader = csv.reader(f)
+                reader.__next__()  # Skipping first row
+                for row in reader:
+                    transaction_id, product, quantity, customer, date = row
+                    quantity = int(quantity)
+                    date = parse_date(date)
+
+                    try:
+                        product_obj = Product.objects.get(name__iexact=product)
+                    except:
+                        product_obj = None
+
+                    if product_obj is not None:
+                        customer_obj, _ = Customer.objects.get_or_create(
+                            name=customer)  # _ in place for created boolean
+                        salesman_obj = Profile.objects.get(user=request.user)
+                        position_obj = Position.objects.create(
+                            product=product_obj, quantity=quantity, created=date)
+                        sale_obj, _ = Sale.objects.get_or_create(
+                            transaction_id=transaction_id, customer=customer_obj,
+                            salesman=salesman_obj, created=date)
+                        sale_obj.positions.add(position_obj)
+                        sale_obj.save()
+                return JsonResponse({'ex': False})
+        else:
+            return JsonResponse({'ex': True})
     return HttpResponse()
